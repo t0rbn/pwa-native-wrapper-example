dfe367bcd01bcfb7f936db354fbdc27c12c33408
Torben Reetz <torben.reetz@mindmatters.de>
2025-05-03 00:36:38 +0200

    initial commit

---
+++ native-wrapper/app/src/main/java/com/example/native_wrapper/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.native_wrapper;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        WebView myWebView = findViewById(R.id.webview);
+//        myWebView.getSettings().setTextZoom(75);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.loadUrl("192.168.0.103:3000");
+    }
+}
